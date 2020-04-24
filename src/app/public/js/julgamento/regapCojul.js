bd0720568e8f1ee2941b12f7c7b78b31761809bb
musashisama <48837659+musashisama@users.noreply.github.com>
2020-04-24 00:09:41 -0300

    Download de todas as colunas

--- src/app/public/js/julgamento/regapCojul.js
+++ src/app/public/js/julgamento/regapCojul.js
@@ -58,24 +58,25 @@ function dataTable(msg) {
         initialSort: [{ column: "Dias_na_Atividade", dir: "desc" }],
         columns: [
             { formatter: "responsiveCollapse", width: 30, minWidth: 30, hozAlign: "left", resizable: false, headerSort: false },
-            { title: "CPF", field: "CPF", sorter: "string", hozAlign: "left", headerFilter: "input", editor: false, responsive: 0, formatter: formatNome },
-            { title: "Responsável Atual", field: "nome", sorter: "string", hozAlign: "left", headerFilter: "input", editor: false, responsive: 0 },
-            { title: "Turma", field: "turma", sorter: "string", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2 },
-            { title: "Câmara", field: "camara", sorter: "string", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2 },
-            { title: "Seção", field: "setor", sorter: "string", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2 },
-            { title: "Contribuinte", field: "Contribuinte", sorter: "string", hozAlign: "center", editor: false, responsive: 2 },
-            { title: "Processo", field: "Processo", sorter: "number", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2 },
-            { title: "Ind. Apenso", field: "Ind_Apenso", sorter: "string", hozAlign: "center", editor: false, responsive: 2 },
-            { title: "Atividade", field: "Atividade", sorter: "string", hozAlign: "center", headerFilter: "input", editor: false, responsive: 0 },
-            { title: "Situação de Julgamento", field: "Situacao", sorter: "string", headerFilter: "input", bottomCalc: "count", hozAlign: "center", editor: false, responsive: 0 },
-            { title: "Entrada na Atividade", field: "Entrada_na_Atividade", sorter: "date", hozAlign: "center", editor: false, responsive: 2 },
-            { title: "Horas CARF", field: "HE_CARF", sorter: "number", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2 },
-            { title: "Dias na Atividade", field: "Dias_na_Atividade", formatter: coloreDias, sorter: "number", hozAlign: "center", editor: false, responsive: 0 },
-            { title: "Dias da Sessão de Julgamento", field: "Dias_da_SJ", sorter: "number", hozAlign: "center", editor: false, responsive: 2 },
-            { title: "Data da Sessão de Julgamento", field: "Data_da_Sessao_Julgamento", sorter: "number", hozAlign: "center", editor: false, responsive: 2 },
-            { title: "Dias da Última Distribuição", field: "Dias_da_Dist", sorter: "number", hozAlign: "center", editor: false, responsive: 2 },
-            { title: "Retorno Sepoj?", field: "Retorno_Sepoj", sorter: "string", hozAlign: "center", editor: false, responsive: 0 },
-            { title: "Última Equipe", field: "Equipe_Ultima", sorter: "string", hozAlign: "center", editor: false, responsive: 2 },
+            { title: "CPF", field: "CPF", sorter: "string", hozAlign: "left", headerFilter: "input", editor: false, responsive: 0, formatter: formatNome, download: true, },
+            { title: "Responsável Atual", field: "nome", sorter: "string", hozAlign: "left", headerFilter: "input", editor: false, responsive: 0, download: true, },
+            { title: "Turma", field: "turma", sorter: "string", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2, download: true, },
+            { title: "Câmara", field: "camara", sorter: "string", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2, download: true, },
+            { title: "Seção", field: "setor", sorter: "string", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2, download: true, },
+            { title: "Contribuinte", field: "Contribuinte", sorter: "string", hozAlign: "center", editor: false, responsive: 2, download: true, },
+            { title: "Processo", field: "Processo", sorter: "number", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2, download: true, },
+            { title: "Ind. Apenso", field: "Ind_Apenso", sorter: "string", hozAlign: "center", editor: false, responsive: 2, download: true, },
+            { title: "Atividade", field: "Atividade", sorter: "string", hozAlign: "center", headerFilter: "input", editor: false, responsive: 0, download: true, },
+            { title: "Situação de Julgamento", field: "Situacao", sorter: "string", headerFilter: "input", bottomCalc: "count", hozAlign: "center", editor: false, responsive: 0, download: true, },
+            { title: "Entrada na Atividade", field: "Entrada_na_Atividade", sorter: "date", hozAlign: "center", editor: false, responsive: 2, download: true, },
+            { title: "Horas CARF", field: "HE_CARF", sorter: "number", hozAlign: "center", headerFilter: "input", editor: false, responsive: 2, download: true, },
+            { title: "Dias na Atividade", field: "Dias_na_Atividade", formatter: coloreDias, sorter: "number", hozAlign: "center", editor: false, responsive: 0, download: true, },
+            { title: "Dias da Sessão de Julgamento", field: "Dias_da_SJ", sorter: "number", hozAlign: "center", editor: false, responsive: 2, download: true, },
+            { title: "Data da Sessão de Julgamento", field: "Data_da_Sessao_Julgamento", sorter: "number", hozAlign: "center", editor: false, responsive: 2, download: true, },
+            { title: "Dias da Última Distribuição", field: "Dias_da_Dist", sorter: "number", hozAlign: "center", editor: false, responsive: 2, download: true, },
+            { title: "Retorno Sepoj?", field: "Retorno_Sepoj", sorter: "string", hozAlign: "center", editor: false, responsive: 0, download: true, },
+            { title: "Última Equipe", field: "Equipe_Ultima", sorter: "string", hozAlign: "center", editor: false, responsive: 2, download: true, },
+            { title: "Observações", field: "Observacoes", sorter: "string", hozAlign: "center", editor: false, responsive: 1, download: true, },
         ],
         autoColumns: false,
         locale: true,
@@ -126,7 +127,7 @@ let formatNome = function formatNome(cell) {
 //         if((cell.getRow().getData().Ind_Apenso == 'N' || (cell.getRow().getData().Questionamento_CARF != '' && cell.getRow().getData().Ind_Apenso != 'S')) && (cell.getRow().getData().Atividade == 'Para Relatar' && (value < 180&&value>=140))) { cell.getElement().style.color = 'rgb(245, 131, 0)'; cell.getElement().style.fontWeight = 'bolder';}        
 //         if((cell.getRow().getData().Ind_Apenso == 'N' || (cell.getRow().getData().Questionamento_CARF != '' && cell.getRow().getData().Ind_Apenso != 'S')) && (cell.getRow().getData().Atividade == 'Para Relatar' && value <140)){ cell.getElement().style.color = 'rgb(63, 138, 2)';cell.getElement().style.fontWeight = 'bolder'}      
 //       }
-      
+
 //       if((cell.getRow().getData().Ind_Apenso == 'N' || (cell.getRow().getData().Questionamento_CARF != '' && cell.getRow().getData().Ind_Apenso != 'S')) && (cell.getRow().getData().Atividade == 'Formalizar Decisao' && value) >= 30){ cell.getElement().style.color = '#D8000C';cell.getElement().style.fontWeight = 'bolder'}
 //       if((cell.getRow().getData().Ind_Apenso == 'N' || (cell.getRow().getData().Questionamento_CARF != '' && cell.getRow().getData().Ind_Apenso != 'S')) && (cell.getRow().getData().Atividade == 'Formalizar Decisao' && value) < 30){ cell.getElement().style.color = 'rgb(245, 131, 0)';cell.getElement().style.fontWeight = 'bolder'}
 //       if((cell.getRow().getData().Ind_Apenso == 'N' || (cell.getRow().getData().Questionamento_CARF != '' && cell.getRow().getData().Ind_Apenso != 'S')) && (cell.getRow().getData().Atividade == 'Formalizar Voto Vencedor' && value) >= 30){ cell.getElement().style.color = '#D8000C';cell.getElement().style.fontWeight = 'bolder'}
@@ -135,7 +136,7 @@ let formatNome = function formatNome(cell) {
 //       if((cell.getRow().getData().Ind_Apenso == 'N' || (cell.getRow().getData().Questionamento_CARF != '' && cell.getRow().getData().Ind_Apenso != 'S')) && (cell.getRow().getData().Atividade == 'Apreciar e Assinar Documento' && value) >= 15){ cell.getElement().style.color = '#D8000C';cell.getElement().style.fontWeight = 'bolder'}
 //       if((cell.getRow().getData().Ind_Apenso == 'N' || (cell.getRow().getData().Questionamento_CARF != '' && cell.getRow().getData().Ind_Apenso != 'S')) && (cell.getRow().getData().Atividade == 'Apreciar e Assinar Documento' && value) < 15){ cell.getElement().style.color = 'rgb(245, 131, 0)';cell.getElement().style.fontWeight = 'bolder'}
 //       if((cell.getRow().getData().Ind_Apenso == 'N' || (cell.getRow().getData().Questionamento_CARF != '' && cell.getRow().getData().Ind_Apenso != 'S')) && (cell.getRow().getData().Atividade == 'Corrigir Decisão' && value) >= 1){ cell.getElement().style.color = '#D8000C';cell.getElement().style.fontWeight = 'bolder'}
-  
+
 //     return value
 // }
 
@@ -143,19 +144,19 @@ function coloreDias(cell, formatterParams) {
     let value = cell.getValue();
 
     if (cell.getRow().getData().Atividade == 'Para Relatar' && cell.getRow().getData().Situacao == 'AGUARDANDO PAUTA') {
-        if(value >= 180){ cell.getElement().style.color = '#D8000C';cell.getElement().style.fontWeight = 'bolder'}
-        if(value < 180&&value>=140) { cell.getElement().style.color = 'rgb(245, 131, 0)'; cell.getElement().style.fontWeight = 'bolder';}        
-        if(value <140){ cell.getElement().style.color = 'rgb(63, 138, 2)';cell.getElement().style.fontWeight = 'bolder'}      
-      }
-      
-      if(cell.getRow().getData().Atividade == 'Formalizar Decisao' && value >= 30){ cell.getElement().style.color = '#D8000C';cell.getElement().style.fontWeight = 'bolder'}
-      if(cell.getRow().getData().Atividade == 'Formalizar Decisao' && value < 30){ cell.getElement().style.color = 'rgb(245, 131, 0)';cell.getElement().style.fontWeight = 'bolder'}
-      if(cell.getRow().getData().Atividade == 'Formalizar Voto Vencedor' && value >= 30){ cell.getElement().style.color = '#D8000C';cell.getElement().style.fontWeight = 'bolder'}
-      if(cell.getRow().getData().Atividade == 'Formalizar Voto Vencedor' && value < 30){ cell.getElement().style.color = 'rgb(245, 131, 0)';cell.getElement().style.fontWeight = 'bolder'}
+        if (value >= 180) { cell.getElement().style.color = '#D8000C'; cell.getElement().style.fontWeight = 'bolder' }
+        if (value < 180 && value >= 140) { cell.getElement().style.color = 'rgb(245, 131, 0)'; cell.getElement().style.fontWeight = 'bolder'; }
+        if (value < 140) { cell.getElement().style.color = 'rgb(63, 138, 2)'; cell.getElement().style.fontWeight = 'bolder' }
+    }
 
-      if(cell.getRow().getData().Atividade == 'Apreciar e Assinar Documento' && value >= 15){ cell.getElement().style.color = '#D8000C';cell.getElement().style.fontWeight = 'bolder'}
-      if(cell.getRow().getData().Atividade == 'Apreciar e Assinar Documento' && value < 15){ cell.getElement().style.color = 'rgb(245, 131, 0)';cell.getElement().style.fontWeight = 'bolder'}
-      if(cell.getRow().getData().Atividade == 'Corrigir Decisão' && value >= 1){ cell.getElement().style.color = '#D8000C';cell.getElement().style.fontWeight = 'bolder'}
-  
+    if (cell.getRow().getData().Atividade == 'Formalizar Decisao' && value >= 30) { cell.getElement().style.color = '#D8000C'; cell.getElement().style.fontWeight = 'bolder' }
+    if (cell.getRow().getData().Atividade == 'Formalizar Decisao' && value < 30) { cell.getElement().style.color = 'rgb(245, 131, 0)'; cell.getElement().style.fontWeight = 'bolder' }
+    if (cell.getRow().getData().Atividade == 'Formalizar Voto Vencedor' && value >= 30) { cell.getElement().style.color = '#D8000C'; cell.getElement().style.fontWeight = 'bolder' }
+    if (cell.getRow().getData().Atividade == 'Formalizar Voto Vencedor' && value < 30) { cell.getElement().style.color = 'rgb(245, 131, 0)'; cell.getElement().style.fontWeight = 'bolder' }
+
+    if (cell.getRow().getData().Atividade == 'Apreciar e Assinar Documento' && value >= 15) { cell.getElement().style.color = '#D8000C'; cell.getElement().style.fontWeight = 'bolder' }
+    if (cell.getRow().getData().Atividade == 'Apreciar e Assinar Documento' && value < 15) { cell.getElement().style.color = 'rgb(245, 131, 0)'; cell.getElement().style.fontWeight = 'bolder' }
+    if (cell.getRow().getData().Atividade == 'Corrigir Decisão' && value >= 1) { cell.getElement().style.color = '#D8000C'; cell.getElement().style.fontWeight = 'bolder' }
+
     return value
 }
