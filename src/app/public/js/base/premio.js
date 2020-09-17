eacf90c8b055b128e510329943ff05dcd540c2e9
musashisama <48837659+musashisama@users.noreply.github.com>
2020-09-17 07:44:46 -0300

    votacao Premio

--- src/app/public/js/base/premio.js
+++ src/app/public/js/base/premio.js
@@ -4,6 +4,7 @@ function inicializaComponentes() {
   $(document).ready(function () {
     initSelect();
     initBtns();
+    sendVote();
   });
 }
 
@@ -13,7 +14,6 @@ function initSelect() {
 
 function initBtns() {
   $('.btnSelCons').click((e) => {
-    console.log($('.selectCons option:selected').text());
     $('#nomeCons').text($('.selectCons option:selected').text());
   });
   $('.btnSelServ').click((e) => {
@@ -31,5 +31,43 @@ function initBtns() {
   $('.btnCancelTerc').click((e) => {
     $('#nomeTerc').text('');
   });
-  //<span id="nomeTerc"/> btnSelCons btnCancelCons
+}
+
+function sendVote() {
+  $('.btnVotar').click((e) => {
+    var cookie = document.cookie;
+    if (cookie.includes('voto=true')) {
+      var toastHTML = `<span>Você já votou e não poderá fazê-lo novamente.</span>`;
+      M.toast({ html: toastHTML, classes: 'rounded', timeRemaining: 500 });
+    } else {
+      let voto = {
+        cons: $('#nomeCons').text(),
+        serv: $('#nomeServ').text(),
+        terc: $('#nomeTerc').text(),
+        uniqueId: moment.now(),
+        dtVoto: moment().format('DD/MM/YYYY'),
+      };
+      $.ajax({
+        url: `/votacao-premio`,
+        type: 'POST',
+        data: voto,
+        beforeSend: function () {
+          $('.btnVotar').toggle();
+        },
+      })
+        .done(function (msg) {
+          var toastHTML = `<span>Voto registrado com sucesso!<br/> Aguarde redirecionamento.</span>`;
+          M.toast({ html: toastHTML, classes: 'rounded', timeRemaining: 500 });
+          document.cookie = 'voto=true';
+          setTimeout(function () {
+            location.href = `/`;
+          }, 2500);
+        })
+        .fail(function (jqXHR, textStatus, msg) {
+          $('.btnVotar').toggle();
+          var toastHTML = `<span>Ocorreu um erro.</span>`;
+          M.toast({ html: toastHTML, classes: 'rounded', timeRemaining: 500 });
+        });
+    }
+  });
 }
