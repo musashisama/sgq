651d624c3c7a3c2e1512942e0aefabf36c7f859a
musashisama <48837659+musashisama@users.noreply.github.com>
2020-11-26 14:49:15 -0300

    Ajustes Gestão Solicitações (faltou initSelect())

--- src/app/public/js/julgamento/gestaoregsolicitacoes.js
+++ src/app/public/js/julgamento/gestaoregsolicitacoes.js
@@ -11,7 +11,12 @@ function inicializaComponentes() {
     tabelaSolicitacoes();
     initOpcoes();
     initElementos();
+    initSelect();
   });
+}
+
+function initSelect() {
+  $('select').formSelect();
 }
 
 function initElementos() {
