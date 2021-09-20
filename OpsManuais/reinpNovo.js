a9bac755d3e1303ea84b6b8098e6597ca950830b
Paulo de Carvalho <prjunior@gmail.com>
2021-09-20 15:54:35 -0300

    Formatador Reinp novo

---
+++ OpsManuais/reinpNovo.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const d3 = require('d3');
+
+let reinp2020,
+  conselheiros = [];
+reinp2020 = fs.readFileSync('./relatorios/REINPCOMPLETO.csv', 'utf8');
+conselheiros = fs.readFileSync('./relatorios/Conselheiros.csv', 'utf8');
+
+let rel = d3.csvParse(reinp2020);
+let cons = d3.csvParse(conselheiros);
+
+rel.forEach((r) => {
+  cons.forEach((c) => {
+    if (r.cpf == c.cpf) {
+      r.unidade = c.unidade;
+    }
+  });
+});
+
+rel.forEach((r) => {
+  r.he = +r.he + +r.apes749;
+  if (r.apes749 != '') {
+    r.obs = `Apuração Especial 749: ${r.apes749} horas.`;
+  }
+});
+
+fs.writeFileSync(
+  './relatorios/REINP_COMPLETO_FORMATADO.csv',
+  d3.csvFormat(rel),
+);
+console.log(rel);
