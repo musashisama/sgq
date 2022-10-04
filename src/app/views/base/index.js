d369f9ef7c065b600b22e9ff2c4e7f1e48e7571f
Paulo de Carvalho <48837659+musashisama@users.noreply.github.com>
2022-10-04 18:58:27 -0300

    Acompanhamento Premio

--- src/app/views/base/index.js
+++ src/app/views/base/index.js
@@ -2,6 +2,7 @@ module.exports = {
   principal: require('./principal/principal.marko'),
   login: require('./login/login.marko'),
   premio: require('./premio/premio.marko'),
+  votacaoPremio: require('./premio/votacao-premio.marko'),
   trocasenha: require('./trocasenha/trocasenha.marko'),
   erro403: require('./erros/403.marko'),
   erro404: require('./erros/404.marko'),
